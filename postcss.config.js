ca80199e63e4c2174e4ddd250a08191e0987d271
karol-wojak <wojak.karol95@gmail.com>
2025-06-17 18:47:49 +0200

    Fix missing imports issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,13 @@
 // postcss.config.js
+// Import autoprefixer here
+import autoprefixer from 'autoprefixer';
+// Import the Tailwind CSS PostCSS plugin
+import tailwindcss from '@tailwindcss/postcss';
+
 export default {
   plugins: [
-    // In Tailwind CSS v4, the @tailwindcss/postcss plugin handles everything.
-    "@tailwindcss/postcss",
+    // In Tailwind CSS v4, the @tailwindcss/postcss plugin needs to be called as a function.
+    tailwindcss(), // Correctly invoking the plugin
     // Autoprefixer is still needed for vendor prefixes.
     autoprefixer,
   ],
