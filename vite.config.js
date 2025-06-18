074c1709510a4c83fcd62ca4a2e899289baecb6f
karol-wojak <wojak.karol95@gmail.com>
2025-06-18 19:55:19 +0200

    Change base url

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+    import { defineConfig } from 'vite'
+    import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/real-estate-project/',
-});
+    // Get the repository name from the environment variable (if set for CI)
+    // or hardcode it for consistent GitHub Pages deployment.
+    // Replace 'your-repository-name' with your actual GitHub repository name.
+    const BASE_URL = process.env.VITE_BASE_URL || '/real_estate_website/';
+
+    // https://vitejs.dev/config/
+    export default defineConfig({
+      plugins: [react()],
+      base: BASE_URL, // Set the base URL for deployment
+    })
+    
