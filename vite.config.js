12adf39e2e517fd6f7ac2a20641ca98160fff3f0
karol-wojak <wojak.karol95@gmail.com>
2025-06-28 11:56:46 +0200

    Change the base URL to the root of the domain

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@
     // Get the repository name from the environment variable (if set for CI)
     // or hardcode it for consistent GitHub Pages deployment.
     // Replace 'your-repository-name' with your actual GitHub repository name.
-    const BASE_URL = process.env.VITE_BASE_URL || '/real_estate_website/';
+    // const BASE_URL = process.env.VITE_BASE_URL || '/real_estate_website/';
 
     // https://vitejs.dev/config/
     export default defineConfig({
       plugins: [react()],
-      base: BASE_URL, // Set the base URL for deployment
+      base: '/',
     })
     
